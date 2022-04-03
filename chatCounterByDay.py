2da378dfa16ec0389dda15f28ae0920327d77280
Schwartzblat <alon.ponch@gmail.com>
2022-04-03 22:19:59 +0300

    update

--- chatCounterByDay.py
+++ chatCounterByDay.py
@@ -1,6 +1,8 @@
 import timeit
 import xlsxwriter
+import re
 
+pattern = re.compile('[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{4}')
 file = open("filename", mode="r", encoding="utf-8")
 lines = file.readlines()
 file.close()
@@ -8,7 +10,7 @@ datesCounter = dict({})
 for line in lines:
     try:
         date = line.strip().split("[")[1].split(",")[0]
-        if date.count("/") == 2:
+        if pattern.match(date):
             if datesCounter.get(date) is not None:
                 datesCounter[date] += 1
             else:
@@ -16,22 +18,21 @@ for line in lines:
     except:
         pass
 
-
 dates = list(datesCounter.keys())
 values = list(datesCounter.values())
 f = open("result.txt", mode="w")
 for i in range(len(dates)):
-    f.write(str(dates[i])+":"+str(values[i])+"\n")
+    f.write(str(dates[i]) + ":" + str(values[i]) + "\n")
 f.close()
-workbook = xlsxwriter.Workbook('Expenses01.xlsx')
+workbook = xlsxwriter.Workbook('messageTableByDay.xlsx')
 worksheet = workbook.add_worksheet()
 row = 0
 col = 0
 # Iterate over the data and write it out row by row.
 for i in range(len(dates)):
     item = dates[i]
-    cost=values[i]
-    worksheet.write(row, col,     item)
+    cost = values[i]
+    worksheet.write(row, col, item)
     worksheet.write(row, col + 1, cost)
     row += 1
 workbook.close()
